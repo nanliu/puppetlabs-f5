0b9091ea1cd4f592b46edddfcc7fc64f9f3357f6
Nan Liu <nan@puppetlabs.com>
2011-12-28 17:36:00 -0800

    Complete F5 profileauth implementation.
    * update documentation to match F5 icontrol docs.
    * add validation for most resource attributes.
    * complete create method.

--- lib/puppet/provider/f5_profileauth/f5_profileauth.rb
+++ lib/puppet/provider/f5_profileauth/f5_profileauth.rb
@@ -33,7 +33,9 @@ Puppet::Type.type(:f5_profileauth).provide(:f5_profileauth, :parent => Puppet::P
     define_method(method.to_sym) do
       if transport[wsdl].respond_to?("get_#{method}".to_sym)
         profile_string = transport[wsdl].send("get_#{method}", resource[:name]).first
-        {"value" => profile_string.value, "default_flag" => profile_string.default_flag}
+
+        { "value"        => profile_string.value.to_s,
+          "default_flag" => profile_string.default_flag}
       end
     end
   end
@@ -41,20 +43,39 @@ Puppet::Type.type(:f5_profileauth).provide(:f5_profileauth, :parent => Puppet::P
   methods.each do |method|
     define_method("#{method}=") do |profile_string|
       if transport[wsdl].respond_to?("set_#{method}".to_sym)
-        transport[wsdl].send("set_#{method}",
-                              resource[:name],
-                              [:value => profile_string["value"],
-                               :default_flag => profile_string["default_flag"]])
+        transport[wsdl].send("set_#{method}", resource[:name],
+                              [ :value        => profile_string["value"],
+                                :default_flag => profile_string["default_flag"] ] )
       end
     end
   end
 
+  # default profile is the only attribute that does not support default_flag.
+  def default_profile
+    transport[wsdl].get_default_profile(resource[:name]).first
+  end
+
+  def default_profile=(value)
+    transport[wsdl].set_default_profile(resource[:name], resource[:default_profile])
+  end
+
   def create
     Puppet.debug("Puppet::Provider::F5_ProfileAuth: creating profile #{resource[:name]}")
+
+  #  require 'ruby-debug'
+  #  Debugger.start
+  #  debugger
+
+    transport[wsdl].create([resource[:name]],
+                           [{ :value        => resource[:configuration_name]['value'],
+                              :default_flag => resource[:configuration_name]['default_flag'] }],
+                           [{ :value        => resource[:authentication_method]['value'],
+                              :default_flag => resource[:authentication_method]['default_flag'] }])
   end
 
   def destroy
     Puppet.debug("Puppet::Provider::F5_ProfileAuth: destroying profile #{resource[:name]}")
+
     transport[wsdl].delete_profile([resource[:name]])
   end
 
--- lib/puppet/type/f5_profileauth.rb
+++ lib/puppet/type/f5_profileauth.rb
@@ -1,7 +1,7 @@
 Puppet::Type.newtype(:f5_profileauth) do
   @doc = "Manage F5 auth profiles."
 
-  apply_to_device
+#  apply_to_device
 
   ensurable do
     desc "Manage F5 auth profile."
@@ -18,30 +18,63 @@ Puppet::Type.newtype(:f5_profileauth) do
   end
 
   newparam(:name, :namevar=>true) do
-    desc "The auth profile name."
+    desc "The ProfileAuth interface enables you to manipulate a local load
+    balancer's authentication profile"
   end
 
   newproperty(:authentication_method) do
-    desc "The auth profile authentication method."
+    desc "The authentication methods that the specified profiles will be
+    using."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfileAuth: authentication_method value must be /^AUTHENTICATION_METHOD_(LDAP|RADIUS|SSL_CC_LDAP|SSL_OCSP|TACACS|GENERIC|SSL_CRLDP)$/.' unless value['value'] =~ /^AUTHENTICATION_METHOD_(LDAP|RADIUS|SSL_CC_LDAP|SSL_OCSP|TACACS|GENERIC|SSL_CRLDP)$/
+    end
   end
 
   newproperty(:configuration_name) do
-    desc "The auth profile configuration name."
+    desc "The names of the authentication configurations that the specified
+    profiles will be using."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfilePersistence: configuration_name value must be type string.' unless value['value'].is_a? String
+    end
   end
 
   newproperty(:credential_source) do
-    desc "The auth profile credential source."
+    desc "The sources of the credentials that the specified profiles will be
+    using."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfileAuth: credential_source value must be /^CREDENTIAL_SOURCE_HTTP_BASIC_AUTH$/' unless value['value'] =~ /^CREDENTIAL_SOURCE_HTTP_BASIC_AUTH$/
+    end
+  end
+
+  newproperty(:default_profile) do
+    desc "The names of the default profiles from which the specified profiles
+    will derive default values for its attributes."
   end
 
   newproperty(:idle_timeout) do
-    desc "The auth profile idle timeout."
+    desc "The idle timeout for the specified auth profiles."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfilePersistence: idle_timeout value must be /^\d+/.' unless value['value'] =~ /^\d+/
+    end
   end
 
   newproperty(:profile_mode) do
-    desc "The auth profile profile mode."
+    desc "The modes for the specified auth profiles."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfileAuth: profile_mode value must be /^PROFILE_MODE_(DISABLED|ENABLED)$/' unless value['value'] =~ /^PROFILE_MODE_(DISABLED|ENABLED)$/
+    end
   end
 
   newproperty(:rule_name) do
-    desc "The auth profile rule name."
+    desc "The names of rules that the specified profiles will be using."
+
+    validate do |value|
+      raise Puppet::Error, 'Pupppet::Type::F5_ProfilePersistence: rule_name value must be type string.' unless value['value'].is_a? String
+    end
   end
 end
